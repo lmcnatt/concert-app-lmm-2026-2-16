375a64a7b5b73cd445762bdca6270de9a87d0dda
lmcnatt <logan.m.mcnatt@gmail.com>
2026-02-16 22:51:44 +0000

    debug

--- scripts/app_stop.sh
+++ scripts/app_stop.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
+echo "--- DEBUG START ---"
+echo "Current User: $(whoami)"
+echo "Path BEFORE nvm: $PATH"
+
 cd /home/ec2-user/app
 export NVM_DIR="/home/ec2-user/.nvm" 
+
+echo "Path AFTER nvm: $PATH"
+which node || echo "Node not found in path"
+which pm2 || echo "PM2 not found in path"
+echo "--- DEBUG END ---"
+
 [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
 pm2 stop index.js
